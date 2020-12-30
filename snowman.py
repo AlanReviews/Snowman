f417b2ff1254386e89f80bcb5379c776cbde41bc
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2020-12-29 21:33:55 -0500

    Added a black top hat

--- snowman.py
+++ snowman.py
@@ -6,12 +6,21 @@ DISPLAY = pygame.display.set_mode((600,600),0,32)
 
 SKY = (135, 206, 235)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 DISPLAY.fill(SKY)
 
 while True:
-    pygame.draw.circle(DISPLAY, WHITE, (200, 350), 50)
+    # Drawing the body of the snowman
     pygame.draw.circle(DISPLAY, WHITE, (200, 500), 100)
+    pygame.draw.circle(DISPLAY, WHITE, (200, 350), 50)
+    pygame.draw.circle(DISPLAY, WHITE, (200, 275), 25)
+    
+    # Drawing the hat of the snowman
+    pygame.draw.line(DISPLAY, BLACK, (150, 250), (250, 250))
+    pygame.draw.rect(DISPLAY, BLACK, (175, 200, 50, 50))
+
+    # Making the program display the image until the user chooses to close it
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
