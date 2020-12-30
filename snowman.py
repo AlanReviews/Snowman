5f1308e0fc5db9ee967930d9d7e6f1cb5da8e414
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2020-12-30 09:39:05 -0500

    Added arms

--- snowman.py
+++ snowman.py
@@ -7,6 +7,7 @@ DISPLAY = pygame.display.set_mode((600,600),0,32)
 SKY = (135, 206, 235)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BROWN = (139,69,19)
 
 DISPLAY.fill(SKY)
 
@@ -19,6 +20,11 @@ while True:
     # Drawing the hat of the snowman
     pygame.draw.line(DISPLAY, BLACK, (150, 250), (250, 250))
     pygame.draw.rect(DISPLAY, BLACK, (175, 200, 50, 50))
+
+    # Drawing the left arm
+    pygame.draw.line(DISPLAY, BROWN, (75, 275), (150, 350))
+    # Drawing the right arm
+    pygame.draw.line(DISPLAY, BROWN, (250, 350), (325, 275))
 
     # Making the program display the image until the user chooses to close it
     for event in pygame.event.get():
