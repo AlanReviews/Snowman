bbbc2e4fd7ddd713f269bfba3a5df73191e9e188
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2020-12-30 09:52:03 -0500

    Updated Code
    - Added an orange nose
    - Added more comments

--- snowman.py
+++ snowman.py
@@ -8,14 +8,15 @@ SKY = (135, 206, 235)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BROWN = (139,69,19)
+ORANGE = (255,153,0)
 
 DISPLAY.fill(SKY)
 
 while True:
     # Drawing the body of the snowman
-    pygame.draw.circle(DISPLAY, WHITE, (200, 500), 100)
-    pygame.draw.circle(DISPLAY, WHITE, (200, 350), 50)
-    pygame.draw.circle(DISPLAY, WHITE, (200, 275), 25)
+    pygame.draw.circle(DISPLAY, WHITE, (200, 500), 100) # Draws the bottom
+    pygame.draw.circle(DISPLAY, WHITE, (200, 350), 50) # Draws the stomach
+    pygame.draw.circle(DISPLAY, WHITE, (200, 275), 25) # Draws the head
     
     # Drawing the hat of the snowman
     pygame.draw.line(DISPLAY, BLACK, (150, 250), (250, 250))
@@ -32,12 +33,16 @@ while True:
     pygame.draw.circle(DISPLAY, BLACK, (200, 375), 10)
 
     # Drawing the eyes of the snowman
-    pygame.draw.circle(DISPLAY, BLACK, (190, 270), 5)
-    pygame.draw.circle(DISPLAY, BLACK, (210, 270), 5)
+    pygame.draw.circle(DISPLAY, BLACK, (190, 270), 5) # Left eye
+    pygame.draw.circle(DISPLAY, BLACK, (210, 270), 5) # Right eye
+
+    # Drawing the nose of the snowman
+    pygame.draw.circle(DISPLAY, ORANGE, (200, 275), 7)
 
     # Making the program display the image until the user chooses to close it
     for event in pygame.event.get():
         if event.type==QUIT:
+            pygame.image.save(DISPLAY, "snowman.jpg")
             pygame.quit()
             sys.exit()
     pygame.display.update()
